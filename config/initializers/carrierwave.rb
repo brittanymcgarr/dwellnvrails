d8b5b12503abbaed1d1d2f30ccb2662806a1fd7f
brittanymcgarr <brittanymcgarr@nevada.unr.edu>
2017-02-17 03:38:54 +0000

    Updating AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,17 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
+    CarrierWave.configure do |config|
+    
+      config.fog_credentials = {
+        provider: "AWS",
+        aws_access_key_id: ENV["S3_KEY"],
+        aws_secret_access_key: ENV["S3_SECRET"],
+        #region: 'Northern California'
+    
+      }
+      
+      config.cache_dir = "#{Rails.root}/tmp/uploads" 
+      config.fog_directory = ENV["S3_BUCKET_NAME"]
+      config.fog_public = false
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+    end
 end
