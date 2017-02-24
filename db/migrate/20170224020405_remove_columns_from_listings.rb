1a6ac0cb666a93853b1301dc02c09a94005a57c2
brittanymcgarr <brittanymcgarr@nevada.unr.edu>
2017-02-24 03:49:26 +0000

    Updating rails migrations

--- db/migrate/20170224020405_remove_columns_from_listings.rb
+++ db/migrate/20170224020405_remove_columns_from_listings.rb
@@ -2,17 +2,6 @@ class RemoveColumnsFromListings < ActiveRecord::Migration[5.0]
 
   def change
     remove_column :listings, :main_image
-    remove_column :listings, :image_1
-    remove_column :listings, :image_2
-    remove_column :listings, :image_3
-    remove_column :listings, :image_4
-    remove_column :listings, :image_5
-    remove_column :listings, :image_6
-    remove_column :listings, :image_7
-    remove_column :listings, :image_8
-    remove_column :listings, :image_9
-    remove_column :listings, :image_10
-    remove_column :listings, :image_11
   end
 
 end
